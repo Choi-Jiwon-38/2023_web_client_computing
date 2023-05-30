63b19f61964faaa49c58d46ffa040ed1ecdfa713
죄죄원 <81795729+Choi-Jiwon-38@users.noreply.github.com>
2023-05-30 15:21:24 +0900

    feat: 이벤트 객체(e) 관련 실습

--- 0530/src/App.js
+++ 0530/src/App.js
@@ -1,24 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Body() {
+  function handleOnClick(e) {
+    console.log(e);
+    console.log(e.target.name);
+  }
+  return (
+    <div className="body">
+      <button name="A Btn" onClick={handleOnClick}>
+        A Button
+      </button>
+      <button name="B Btn" onClick={handleOnClick}>
+        B Button
+      </button>
+    </div>
+  )
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul>
+      <Body />
+    </ul>
   );
 }
 
